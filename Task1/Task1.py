e2c6fd839583f444c3d4863c69dc008434006230
Omar Ashraf <omarashraf.zak@gmail.com>
2025-03-15 14:11:06 +0200

    first commit

---
+++ Task1/Task1.py
@@ -0,0 +1,11 @@
+import cv2
+import matplotlib.pyplot as plt
+
+image = cv2.imread("Image for task 1.jpg")
+
+
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+plt.imshow(image_rgb)
+plt.axis("off")
+plt.show()
+
